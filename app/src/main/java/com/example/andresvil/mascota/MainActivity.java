dffc151d37db15c9a4bafc4b7be626ca0abb6215
Andres Villarreal <andresvil@gmail.com>
2016-03-28 17:42:12 -0600

    enable corazon button and add estado textview

--- app/src/main/java/com/example/andresvil/mascota/MainActivity.java
+++ app/src/main/java/com/example/andresvil/mascota/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.andresvil.mascota;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView botella;
+    ImageView corazon;
+    ImageView cofre;
+
+    Boolean corazonClicked = false;
+
+    TextView estado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +30,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
+        // Set animationDrawable to drawable from imageView
+        final AnimationDrawable pulpoAnimado = (AnimationDrawable) imageView.getDrawable();
+
+        // Start animation
+        imageView.post(new Runnable() {
+            @Override
+            public void run() {
+                pulpoAnimado.start();
+            }
+        });
+
+        // Set views
+        botella = (ImageView) findViewById(R.id.botella);
+        corazon = (ImageView) findViewById(R.id.corazon);
+        cofre = (ImageView) findViewById(R.id.tesoro);
+        estado = (TextView) findViewById(R.id.estadoTV);
     }
 
     @Override
@@ -41,5 +70,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void verEstado(View view)
+    {
+        // if corazon has not been clicked
+        if(!corazonClicked) {
+            corazon.setColorFilter(Color.argb(255, 179, 24, 34));
+            estado.setVisibility(View.VISIBLE);
+            corazonClicked = true;
+        }
+
+        // if corazon has already been clicked
+        else {
+            corazon.setColorFilter(Color.argb(255, 0, 0, 0));
+            estado.setVisibility(View.INVISIBLE);
+            corazonClicked = false;
+        }
+    }
+
+    public void verBotella(View view)
+    {
+        corazon.setColorFilter(Color.argb(255, 0, 0, 0));
+        estado.setVisibility(View.INVISIBLE);
+        corazonClicked = false;
     }
 }
