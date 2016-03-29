afa34cabaccec16c9e115920ff1b3911b002db69
Andres Villarreal <andresvil@gmail.com>
2016-03-28 18:24:13 -0600

    enable all 3 menu buttons and add demo listView

--- app/src/main/java/com/example/andresvil/mascota/MainActivity.java
+++ app/src/main/java/com/example/andresvil/mascota/MainActivity.java
@@ -10,8 +10,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +24,12 @@ public class MainActivity extends AppCompatActivity {
     ImageView cofre;
 
     Boolean corazonClicked = false;
+    Boolean botellaClicked = false;
+    Boolean cofreClicked = false;
 
     TextView estado;
+    ListView reflexionesList;
+    ArrayList<String> reflexiones = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +56,31 @@ public class MainActivity extends AppCompatActivity {
         corazon = (ImageView) findViewById(R.id.corazon);
         cofre = (ImageView) findViewById(R.id.tesoro);
         estado = (TextView) findViewById(R.id.estadoTV);
+        reflexionesList = (ListView) findViewById(R.id.reflexionesList);
+
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+        reflexiones.add("Aquí está la reflexión que tengo \n" +
+                "después de haber completado el reto.");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, reflexiones);
+        reflexionesList.setAdapter(adapter);
     }
 
     @Override
@@ -72,13 +105,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    // asignado al icono de corazón
     public void verEstado(View view)
     {
         // if corazon has not been clicked
         if(!corazonClicked) {
+            // botella
+            botellaClicked = false;
+            reflexionesList.setVisibility(View.INVISIBLE);
+            botella.setColorFilter(Color.argb(255, 0, 0, 0));
+
+            // corazón
             corazon.setColorFilter(Color.argb(255, 179, 24, 34));
             estado.setVisibility(View.VISIBLE);
             corazonClicked = true;
+
+            // cofre
+            cofreClicked = false;
+            cofre.setColorFilter(Color.argb(255, 0, 0, 0));
         }
 
         // if corazon has already been clicked
@@ -89,10 +133,58 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void verBotella(View view)
+    // asignado al icono de botella
+    public void verReflexiones(View view)
     {
-        corazon.setColorFilter(Color.argb(255, 0, 0, 0));
-        estado.setVisibility(View.INVISIBLE);
-        corazonClicked = false;
+        // if botella has not been clicked
+        if(!botellaClicked) {
+            // botella
+            botellaClicked = true;
+            botella.setColorFilter(Color.argb(255, 179, 24, 34));
+            reflexionesList.setVisibility(View.VISIBLE);
+
+            // corazon
+            corazon.setColorFilter(Color.argb(255, 0, 0, 0));
+            estado.setVisibility(View.INVISIBLE);
+            corazonClicked = false;
+
+            // cofre
+            cofreClicked = false;
+            cofre.setColorFilter(Color.argb(255, 0, 0, 0));
+        }
+
+        // if botella has already been clicked
+        else {
+            botellaClicked = false;
+            botella.setColorFilter(Color.argb(255, 0, 0, 0));
+            reflexionesList.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    // asignado al icono de cofre
+    public void verItems(View view)
+    {
+        // if cofre has not been clicked
+        if(!cofreClicked) {
+            // botella
+            botellaClicked = false;
+            botella.setColorFilter(Color.argb(255, 0, 0, 0));
+            reflexionesList.setVisibility(View.INVISIBLE);
+
+            // corazon
+            corazon.setColorFilter(Color.argb(255, 0, 0, 0));
+            estado.setVisibility(View.INVISIBLE);
+            corazonClicked = false;
+
+            // cofre
+            cofreClicked = true;
+            cofre.setColorFilter(Color.argb(255, 179, 24, 34));
+        }
+
+        // if cofre has already been clicked
+        else {
+            cofreClicked = false;
+            cofre.setColorFilter(Color.argb(255, 0, 0, 0));
+        }
     }
 }
